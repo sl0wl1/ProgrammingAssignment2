73d0203d0fc5c5d68aee44f8e820afd44b76139b
sl0wl1 <56339600+sl0wl1@users.noreply.github.com>
2020-09-23 00:56:46 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,46 @@
+# creates a special_matrix with additional functions
 makeCacheMatrix <- function(x = matrix()) {
         
-        inv <- NULL
-        set <- function(y) {
+        inv <- NULL                     #sets the inversed matrix to NULL
+        # sets the special_matrix and the inverse variable in the parent environment to NULL
+        set <- function(y) {                    
                 x <<- y
                 inverse <<- NULL
         }
         
+        # implements several functions like set special_matrix, calculates the inverse etc
         get <- function() x
         setinverse <- function(inverse) inv <<- inverse
         getinverse <- function() inv
+        # implements a list of functions to the special matrix
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)
 }
 
 cacheSolve <- function(x, ...) {
-        
+#calculates the inverse matrix and checks if there is an inversed matrix already in the cache
+        # if there is an cached matrix the message will tell and returns the cached inversed matrix
         inv <- x$getinverse()
-        if(!is.null(inv)) {
+        if(!is.null(inv)) {                     
                 message("getting cached data")
                 return(inv)
         }
         
+        # if there is no cache, gets the data out of the special_matrix and returns the inversed matrix
         data <- x$get()
         inv <- solve(data, ...)
         x$setinverse(inv)
         inv
 }
 
+# Test the functions from above
+# Set.seed for transparency
+
 set.seed(234)
 data_for_matrix <- rnorm(25)
-matrix <- matrix (data = data_for_matrix, nrow = 5, ncol = 5)
+matrix <- matrix (data = data_for_matrix, nrow = 5, ncol = 5) # create random square matrix
 
-special_matrix <- makeCacheMatrix(matrix)
+special_matrix <- makeCacheMatrix(matrix) # creates special_matrix for caching
 
-cacheSolve(special_matrix)
+cacheSolve(special_matrix) # returns the inverse matrix
